9ff19c49773108758bc28487d9b41304684f0643
KANIKE NANDHINI <kanikenandhini2004@gmail.com>
2026-03-03 12:37:50 +0530

    Enhance student management with error handling and bulk insert
    Added error handling for adding students and implemented bulk insert functionality.

--- student_app.py
+++ student_app.py
@@ -26,17 +26,21 @@ Session=sessionmaker(bind=engine)
 s=Session()
 
 def add_students():
-    id=int(input("Enter student id no:"))
-    name=input("Enter student name:")
-    marks=int(input("Enter student marks:"))
-    if marks >=35:
-        status="Pass"
-    else:
-        status="Fail"
-    student=Student(id=id,name=name,marks=marks,status=status)
-    s.add(student)
-    s.commit()
-    print("Student added successfully")
+    try:
+        id=int(input("Enter student id no:"))
+        name=input("Enter student name:")
+        marks=int(input("Enter student marks:"))
+        if marks >=35:
+            status="Pass"
+        else:
+            status="Fail"
+        student=Student(id=id,name=name,marks=marks,status=status)
+        s.add(student)
+        s.commit()
+        print("Student added successfully")
+    except Exception as e:
+        s.rollback()
+        print("Error:",e)
 
 def view_students():
     students=s.query(Student).all()
@@ -70,14 +74,26 @@ def delete_student():
         print("Student details does not exists")
 def search_student():
     name=input("Enter the student name:")
-    student=s.query(Student).filter_by(name=name).first()
-    if student:
-        print(f"ID:{student.id},status:{student.status}")
+    students=s.query(Student).filter(Student.name.ilike(f"%{name}%")).all()
+    if students:
+        for i in students:
+            print(f"ID:{i.id},Name:{i.name},Marks={i.marks},Status:{i.status}")
     else:
         print("Student not found")
+def bulk_insert():
+    students=[
+        {"id":111,"name":"Ravi","marks":45,"status":"Pass"},
+        {"id":112,"name":"Raju","marks":25,"status":"Fail"},
+        {"id":113,"name":"Ramesh","marks":55,"status":"Pass"},
+        {"id":114,"name":"Suresh","marks":15,"status":"Fail"},
+        {"id":115,"name":"Mahesh","marks":65,"status":"Pass"}
+    ]
+    s.bulk_insert_mappings(Student,students)
+    s.commit()
+    print("Bulk insert completed successfully")
 
 while True:
-    print("\n1.Add Student \n2.View Student \n3.Update Student \n4.Delete Student \n5.Search Student \n6.Exists")
+    print("\n1.Add Student \n2.View Student \n3.Update Student \n4.Delete Student \n5.Search Student \n6.Bulk Insert \n7.Exists")
     choice=int(input("Enter your choice:"))
     if choice==1:
         add_students()
@@ -90,10 +106,10 @@ while True:
     elif choice==5:
         search_student()
     elif choice==6:
+        bulk_insert()
+    elif choice==7:
         print("Exit the programm")
         break
     else:
         print("Enter the valid choice")
-
-
 s.close()
